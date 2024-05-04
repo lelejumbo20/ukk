db3d7257a2a97610f353fda68c525a5acfcb2dbf
lelejumbo20 <168810862+lelejumbo20@users.noreply.github.com>
2024-05-04 12:16:34 +0700

    Add files via upload

---
+++ Kasir-main/index.php
@@ -0,0 +1,60 @@
+<?php
+require 'proses-login.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Halaman Masuk</title>
+    <link href="css/styles.css" rel="stylesheet">
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-primary">
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-7">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header">
+                                    <h3 class="text-center font-weight-light my-4">Selamat Datang Di Aplikasi Kasir</h3>
+                                        <h6 class="text-center font-weight-light my-4">Silahkan Masukan Nama Pengguna Dan Sandi Anda</h6>
+                                </div>
+                                <div class="card-body">
+                                    <form method="post">
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" id="inputEmail" name="username" type="text" placeholder="Enter username" required />
+                                            <label for="inputEmail">Nama Pengguna</label>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" id="inputPassword" name="password" type="password" placeholder="Password" required />
+                                            <label for="inputPassword">Sandi</label>
+                                        </div>
+                                      
+                                        <div class="text-center">
+                                            <button class="btn btn-primary btn-block"  name="proses" type="submit">Masuk</button>
+                                                <hr>
+
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+    <script src="https://code.jquery.com.jquery-3.5.1.sli.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
